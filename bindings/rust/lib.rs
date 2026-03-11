9fe14bea91399543282035090086fec8a868fb6c
Amos Wenger <amos@bearcove.eu>
2026-03-11 18:48:43 +0100

    gitignore target, expose queries etc.

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -2,19 +2,28 @@
 //!
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
 unsafe extern "C" {
-    fn tree_sitter_vixen() -> Language;
+    fn tree_sitter_vixen() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
-pub fn language() -> Language {
-    unsafe { tree_sitter_vixen() }
+/// Get the tree-sitter language function for this grammar.
+pub const fn language() -> LanguageFn {
+    unsafe { LanguageFn::from_raw(tree_sitter_vixen) }
 }
 
 /// The content of the [`node-types.json`][] file for this grammar.
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
+
+/// The Arborium highlights query for Vixen.
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+
+/// The Arborium injections query for Vixen.
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+
+/// Vixen does not currently ship a locals query.
+pub const LOCALS_QUERY: &str = "";
 
 #[cfg(test)]
 mod tests {
@@ -22,7 +31,7 @@ mod tests {
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::language())
+            .set_language(&tree_sitter::Language::new(super::language()))
             .expect("Error loading Vixen grammar");
     }
 }
